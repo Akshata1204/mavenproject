68881cf49880c816ebbc5680a329b9abc20c8756
Akshata~ <akshata1522@gmail.com>
2026-06-22 02:26:51 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello[] with jenkins");
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
